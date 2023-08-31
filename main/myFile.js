18509ab1201a59285aa1647fa0d5c18496bc0180
sushantdhende <sdhende1@gmail.com>
2023-08-31 09:15:10 +0530

    first commit

---
+++ main/myFile.js
@@ -0,0 +1,8 @@
+/** psuedo code for event loop */
+/** when node program starts it creates below arrays to keep track & decide to continue running event loop or not */
+const pendingTimers = [];
+const pendingOS = [];
+const pendingTasks = [];
+
+
+/** exit terminal */
--- main/threads.js
+++ main/threads.js
@@ -1,3 +1,4 @@
+process.env.UV_THREADPOOL_SIZE = 1
 const crypto = require('crypto');
 
 const start = Date.now();
@@ -8,4 +9,36 @@ crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
 
 crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
     console.log('2:', Date.now() - start)
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('3:', Date.now() - start)
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('4:', Date.now() - start)
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('5:', Date.now() - start)
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('6:', Date.now() - start);
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('7:', Date.now() - start)
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('8:', Date.now() - start)
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('9:', Date.now() - start)
+})
+
+crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+    console.log('10:', Date.now() - start)
 })
