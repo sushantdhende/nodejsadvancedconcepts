6e9266cbb7c4da85edbdf8167e7ef90637824ee1
sushantdhende <sdhende1@gmail.com>
2023-09-07 09:20:11 +0530

    section 2 lec 31

--- main/index.js
+++ main/index.js
@@ -1,22 +1,23 @@
+process.env.UV_THREADPOOL_SIZE = 1;
+const crypto = require('crypto');
 const cluster = require('cluster');
 
+/** is the file being executed in master mode */
 if (cluster.isMaster) {
+    /** Cause index.js to be executed *again* but in child mode */
     cluster.fork();
     // cluster.fork();
     // cluster.fork();
     // cluster.fork();
 } else {
+    /** Im a child, Im going to act like a server and do nothing else */
     const express = require('express');
     const app = express();
 
-    function doWork(duration) {
-        let start = Date.now();
-        while (Date.now() - start < duration) {}
-    }
-
     app.get('/', (req, res) => {
-        doWork(5000)
-        res.send('hi there!!');
+        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+            res.send('hi there!!');
+        })
     });
 
     app.get('/fast', (req, res) => {
