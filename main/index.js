ae697252f467619d2917e9a6781aa51f2459254b
sushantdhende <sdhende1@gmail.com>
2023-09-07 10:08:00 +0530

    section 2 lec 37

--- main/index.js
+++ main/index.js
@@ -2,27 +2,30 @@ process.env.UV_THREADPOOL_SIZE = 1;
 const crypto = require('crypto');
 const cluster = require('cluster');
 
-/** is the file being executed in master mode */
-if (cluster.isMaster) {
-    /** Cause index.js to be executed *again* but in child mode */
-    cluster.fork();
-    // cluster.fork();
-    // cluster.fork();
-    // cluster.fork();
-} else {
-    /** Im a child, Im going to act like a server and do nothing else */
-    const express = require('express');
-    const app = express();
+/** Im a child, Im going to act like a server and do nothing else */
+const express = require('express');
+const app = express();
+const Worker = require('webworker-threads').Worker;
 
-    app.get('/', (req, res) => {
-        crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
-            res.send('hi there!!');
-        })
+app.get('/', (req, res) => {
+    const worker = new Worker(function () {
+        this.onmessage = function () {
+            let counter = 0;
+            while (counter < 100000000) counter++;
+
+            postMessage(counter);
+        }
     });
 
-    app.get('/fast', (req, res) => {
-        res.send('This is fast!!');
-    });
+    worker.onmessage = function (myCounter) {
+        console.log('myCounter =>',myCounter);
+    }
 
-    app.listen(3000);
-}
+    worker.postMessage();
+});
+
+app.get('/fast', (req, res) => {
+    res.send('This is fast!!');
+});
+
+app.listen(3000);
